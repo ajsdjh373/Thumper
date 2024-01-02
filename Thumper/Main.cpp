9118f785068b16928fd5008d6e57726f003b9cc6
ajsdjh373 <ndunn24601@gmail.com>
2024-01-01 20:37:51 -0500

    Add project files.

---
+++ Thumper/Main.cpp
@@ -0,0 +1,29 @@
+#include "windows.h"
+
+int CALLBACK WinMain(
+	HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPSTR     lpCmdLine,
+	int       nCmdShow)
+{
+	return 0;
+	/*
+	try
+	{
+		return Game{}.Go();
+	}
+	catch (const TException& e)
+	{
+		MessageBox(nullptr, e.what(), e.GetType(), MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (const std::exception& e)
+	{
+		MessageBox(nullptr, e.what(), "Standard Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	catch (...)
+	{
+		MessageBox(nullptr, "No details available", "Unknown Exception", MB_OK | MB_ICONEXCLAMATION);
+	}
+	return -1;
+	*/
+}
---
+++ Thumper/Standards.h
@@ -0,0 +1,68 @@
+#pragma once
+
+// for h files
+/*
+Author: Nathan Dunn
+Module: GFX_3D
+
+Functional description and intended use
+
+Known bugs and limitations:
+- a
+- b
+- c
+*/
+
+// for cpp files
+/*
+Author: Nathan Dunn
+Module: GFX_3D
+
+See associated header file for more information
+*/
+
+// forward declare classes at the beginning of every header file
+// use the namespace for that module
+namespace EX
+{
+	class EXClass; 
+}
+
+// every class declares members in a specific order: public types, private types, public member functions, private member functions, public variables, private variables
+class EX::EXClass
+{
+public:
+	// public member functions
+	// constructors
+	// destructors
+	// everything else
+
+private:
+	// private member functions
+
+public:
+	// public member variables
+
+private:
+	// private member variables
+
+};
+
+// before functions
+/*
+Description of the function and its intended use
+
+Arguments:
+a		description
+b		description
+c		description
+
+Return:
+a		description
+
+Safeties and known issues:
+- a
+- b
+- c
+
+*/
