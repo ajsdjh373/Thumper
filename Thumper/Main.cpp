5ae0cb0b3aaea9e634bd3cce9f889576e5634cab
ajsdjh373 <ndunn24601@gmail.com>
2024-09-24 19:49:02 -0400

    Fixed window having laggy input response

--- Thumper/Main.cpp
+++ Thumper/Main.cpp
@@ -1,6 +1,8 @@
 #include "WIN_WinWrapper.h"
 #include "WIN_Window.h"
 #include <string>
+#include <chrono>
+#include <thread>
 
 int CALLBACK WinMain(
 	HINSTANCE hInstance,
@@ -10,12 +12,24 @@ int CALLBACK WinMain(
 {
 	try
 	{
+		// temporary clock stuff
+		int inputPeriod_ms = 25;
+		long long lastTime_ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count();
+		long long nextTime_ms = 0;
+
 		WIN::Window testWin;
 		std::string allReceivedKeys = "";
 		testWin.SetTitle(L"test 2");
 		while (true)
 		{
-			// process all messages pending, but to not block for new messages
+			nextTime_ms = lastTime_ms + inputPeriod_ms;
+			if (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::steady_clock::now().time_since_epoch()).count() < nextTime_ms)
+			{
+				std::this_thread::sleep_until(std::chrono::steady_clock::time_point(std::chrono::milliseconds(nextTime_ms)));
+			}
+			lastTime_ms = nextTime_ms;
+
+			// process all messages pending, but not block new messages
 			if (const auto ecode = WIN::Window::ProcessMessageQue())
 			{
 				// if return optional has value, means we're quitting so return exit code
@@ -34,14 +48,18 @@ int CALLBACK WinMain(
 				testWin.SetTitle(L"nah");
 			}
 
+			
+			//throw std::runtime_error("Test error: forced exception"); // test for error handling message box
+
 		};
 
 		return 0;
 
 	}
-	catch (const std::exception&)
+	catch (const std::exception& e)
 	{
 		// otherwise unhandled exception from the rest of the program, constitutes a critical error for the program
+		MessageBoxA(NULL, e.what(), "Error", MB_ICONERROR);
 		return 1;
 	}
 	
--- Thumper/Standards.h
+++ Thumper/Standards.h
@@ -82,4 +82,60 @@ For functions, name spaces, enums, classes:
 FirstSecond_Modifier
 
 Acronyms and shortened words are always all caps
+
+Physical units are separated by an underscore and capitalized per standard notation for the associated field
+Units should be spelled out and treated as words if not paired with an hinting word
+Compount units should be avoided in variable names, but if they must be joined use _ for multiplication and __ for division, eg kg_m__s2
+time_ms		okay, probably milliseconds
+force_MN	okay, MN is a common notation for meganewtons, which are used to measure force
+ship_MN		not okay, it isn't obvious what MN could be
+force_mn	not okay, m is usually representing milli, not mega, and n represents nano, not newton
+Standard notation of prefixes is:
+f	fempto	10e-15
+p	pico	10e-12
+n	nano	10e-9
+u	micro	10e-6
+m	milli	10e-3
+c	centi	10e-2
+h	hecto	10e2
+k	kilo	10e3
+M	mega	10e6
+G	giga	10e9
+T	tera	10e12
+P	peta	10e15
+E	exa		10e18
+Z	zeta	10e21
+Standard units are:
+time		----------
+s		second
+yr		year
+distance	----------
+m		meter
+in		inch
+ft		foot
+mi		mile
+mil		1/1000 of an inch
+micron	don't use microns, use um
+mass		----------
+lbm		pound mass
+g		gram
+slg		slug
+force		----------
+lb		pound force
+N		newton
+tn		short ton, 2000 lb
+lt		long ton, 2240 lb
+t		tonne, 1000 kg
+kip		1000 lb (klb)
+energy		----------
+J		joule
+Wh		watt hour
+btu		british thermal unit
+ftlb	foot pounds (allowable shorthand for ft_lb as it is widely used)
+power		----------
+hp		horse power
+w		watt
+Etc... add any new units here as they are used in the code!
+
+
 */
